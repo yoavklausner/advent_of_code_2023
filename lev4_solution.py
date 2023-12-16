9ee84032cde605b006cd863ed19218eecebc2c2f
yoavklausner <klausner.yoav@gmail.com>
2023-12-17 00:12:31 +0200

    two parts of level4 completed. now have 8 stars

--- lev4_solution.py
+++ lev4_solution.py
@@ -3,12 +3,18 @@
 WINNING_NUMS_COUNT = 10
 MY_NUMS_COUNT = 25
 
+cards_copies_dict = {}
+
+
+def init_dict(cards_count):
+    cards_copies_dict.update({i: 1 for i in range(cards_count)})
+
 
 def print_lev4_first_part_solution():
-
     with open("lev4_input.txt", 'r') as fourth_input_file:
-        total_point = 0
-        for scratch_card in fourth_input_file.readlines():
+        lines = fourth_input_file.readlines()
+        init_dict(len(lines))
+        for i, scratch_card in enumerate(lines):
             card_matches = 0
             winning_nums_set, my_nums = scratch_card.split(':')[1].split('|')
             winning_nums_set = set(filter(str.isdigit, winning_nums_set.split(' ')))
@@ -16,6 +22,6 @@ def print_lev4_first_part_solution():
             for num in my_nums:
                 if num in winning_nums_set:
                     card_matches += 1
-            if card_matches > 0:
-                total_point += 2**(card_matches-1)
-        print(total_point)
+            for row_below in range(i+1, i+1+card_matches):
+                cards_copies_dict[row_below] += cards_copies_dict[i]
+        print(sum(cards_copies_dict.values()))
